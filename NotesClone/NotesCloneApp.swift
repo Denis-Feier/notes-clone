474d046e5336066b51f5a276a07c8a93a0f43dad
DenisuDevForFood <denisfeier98@yahoo.com>
2021-07-16 21:16:29 +0300

    Initial Commit

---
+++ NotesClone/NotesCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotesCloneApp.swift
+//  NotesClone
+//
+//  Created by Denisu on 16.07.2021.
+//
+
+import SwiftUI
+
+@main
+struct NotesCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
